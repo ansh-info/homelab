a9a80eb94927ca8ac3cc80ca1b3df3e3bee97203
ansh-info <anshkumar.info@gmail.com>
2025-11-02 01:23:44 +0100

    feat: add template for secrets
    Co-authored-by: Apoorva Gupta <apoorvaagupta.info@gmail.com>
    
    feat: add template for secrets
    
    Co-authored-by: Apoorva Gupta <apoorvaagupta.info@gmail.com>

---
+++ zshrc/.secrets.zsh
@@ -0,0 +1,28 @@
+# Environment variables
+
+# OPENAI API KEY
+export OPENAI_API_KEY=your_openai_api_key_here
+
+# LangSmith API KEY
+export LANGCHAIN_TRACING_V2=false
+export LANGCHAIN_API_KEY=your_langchain_api_key_here
+
+# Zotero API KEY
+export ZOTERO_API_KEY=your_zotero_api_key_here
+export ZOTERO_USER_ID=your_zotero_user_id_here
+
+# Nvidia API KEY
+export NVIDIA_API_KEY=your_nvidia_api_key_here
+
+# TAVILY_API_KEY
+export TAVILY_API_KEY=your_tavily_api_key_here
+
+# Kaggle API KEY
+export KAGGLE_USERNAME=your_kaggle_username_here
+export KAGGLE_KEY=your_kaggle_key_here
+
+# PYPI
+export PYPI_AUTH_TOKEN=your_pypi_token_here
+
+# and any other sensitive information...
+alias homelab='ssh homelab@<ip_address>'
